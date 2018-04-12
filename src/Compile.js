8384a2ddd93175d7e7f51cab4dc0733b12bb606b
taojunlong <782570386@qq.com>
2018-04-12 23:14:11 +0800

    update
    Change-Id: I56b9a0da8d6eec30411d0ebe152a09ddd7fd0bb8

--- src/Compile.js
+++ src/Compile.js
@@ -1,9 +1,9 @@
 function isElementNode(node) {
-    return node.nodetype === 1
+    return node.nodeType === 1
 }
 
 function isAttributeNode(node) {
-    return node.nodetype === 2
+    return node.nodeType === 2
 }
 
 function isTextNode(node) {
@@ -14,74 +14,77 @@ export default class Compile {
 
     constructor(el, vm) {
         el = document.querySelector(el)
-        this.compileElement(el, vm)
+        this.compileChildNodes(el, vm)
     }
 
-    compileElement(el, vm) {
+    compileChildNodes(el, vm) {
         for (let i = 0, len = el.childNodes.length; i < len; i++) {
             const node = el.childNodes[i]
-            console.log(node.nodeType)
-
             const reg = /\{\{(.*)\}\}/
             const text = node.textContent
-            if (node.nodeType === 3 && reg.test(text)) {
-                if(typeof vm[RegExp.$1] === 'function'){
+            if (isTextNode(node) && reg.test(text)) {
+                if (typeof vm[RegExp.$1] === 'function') {
                     node.textContent = vm[RegExp.$1].call(vm)
-                }else{
+                } else {
                     node.textContent = vm[RegExp.$1]
                 }
+            } else if (isElementNode(node)) {
+                this.compileAttrs(node, vm)
             }
-            if (node.nodeType === 1) {
-                this.compileElement(node, vm)
+
+            if (node.childNodes && node.childNodes.length > 0) {
+                this.compileChildNodes(node, vm)
             }
         }
     }
 
-    node2Fragment(el) {
-        const fragment = document.createDocumentFragment()
-        let child
-        while (child = el.firstChild) {
-            console.log(child)
-            fragment.appendChild(child)
-        }
-        return fragment
-    }
+    // compileChildNodes(el) {
+    //     [].slice.call(el.childNodes).forEach(function (item) {
+    //         const text = item.textContent
+    //         const reg = /\{\{(.*)\}\}/
+    //         if (isElementNode(item)) {
+    //             this.compileAttrs(item)
+    //         } else if (isTextNode(item) && reg.test(text)) {
+    //             this.compileText(item, RegExp.$1)
+    //         }
+    //
+    //         if (item.childNodes && item.childNodes.length) {
+    //             this.compileChildNodes(item)
+    //         }
+    //     })
+    // }
 
-    isElementNode(node) {
-        return node.nodeType == 1;
-    }
-
-    compileChildNodes(el) {
-        [].slice.call(el.childNodes).forEach(function (item) {
-            const text = item.textContent
-            const reg = /\{\{(.*)\}\}/
-            if (isElementNode(item)) {
-                this.compileAttrs(item)
-            } else if (isTextNode(item) && reg.test(text)) {
-                this.compileText(item, RegExp.$1)
-            }
-
-            if (item.childNodes && item.childNodes.length) {
-                this.compileChildNodes(item)
-            }
-        })
-    }
-
-    compileAttrs(node) {
+    compileAttrs(node, vm) {
+        console.log(node)
         var nodeAttrs = node.attributes;
-        [].slice.call(nodeAttrs).forEach(function (item) {
+        [].slice.call(nodeAttrs).forEach(item => {
+            console.log(item)
             var attrName = item.name
+            var attrValue = item.value
             if (!this.isDirective(attrName)) {
                 return;
             }
             if (this.isEventDirective(attrName)) {
-                compileUtil.eventHandler(node, me.$vm, exp, dir)
+                //compileUtil.eventHandler(node, me.$vm, exp, dir)
             } else {
-                compileUtil[dir] && compileUtil[dir](node, me.$vm, exp)
+                var items = attrValue.split('.')
+                var res = vm
+                items.map(item => {
+                    res = res[item]
+                })
+                node.value = res
             }
 
 
         })
+    }
+
+    isDirective(attrName) {
+        return attrName.indexOf('v-') === 0
+    }
+
+    isEventDirective(attName) {
+        return attName.indexOf('v-on:') === 0
     }
 
     compileText(node, exp) {
